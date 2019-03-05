d781a8d489f1eb1603def04a7f3299c52d19ee5c
Стоялов Евгений <levievgen26@gmail.com>
2019-03-05 18:21:43 +0300

    add method to alien calc

--- AliensCombatSystemTest/src/models/calculator/ACCalculator.cs
+++ AliensCombatSystemTest/src/models/calculator/ACCalculator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AliensCombatSystemTest.src.models.targets;
+using AliensCombatSystemTest.src.models.weapon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace AliensCombatSystemTest.src.models.calculator
 {
     abstract class ACCalculator
     {
+        protected enum AccuracityMode
+        {
+            almostMiss, lightHit, strongHit, criticalHit
+        }
+        protected AccuracityMode m_eAccuracity;
+        protected ACTarget m_pTarget;
+        protected ACWeapon m_pWeapon;
+        protected void initializeTarget(ACTarget target)
+        {
+            m_pTarget = target;           
+        }
+        protected void initializeWeapon(ACWeapon weapon)
+        {           
+            m_pWeapon = weapon;
+        }
+         
+
+
     }
 }
--- AliensCombatSystemTest/src/models/calculator/CAlienCalculator.cs
+++ AliensCombatSystemTest/src/models/calculator/CAlienCalculator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AliensCombatSystemTest.src.models.targets;
+using AliensCombatSystemTest.src.models.weapon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,62 @@ namespace AliensCombatSystemTest.src.models.calculator
 {
     class CAlienCalculator:ACCalculator
     {
+
+        public CAlienCalculator(ACTarget target, ACWeapon weapon)
+        {
+            initializeTarget(target);
+            initializeWeapon(weapon);
+            m_eAccuracity = AccuracityMode.almostMiss;
+        }
+        public CAlienCalculator withTarget (ACTarget target)
+        {
+            initializeTarget(target);
+            return this;
+        }
+        public CAlienCalculator withWeapon(ACWeapon weapon)
+        {
+            initializeWeapon(weapon);
+            return this;
+        }
+        public CAlienCalculator almostMissMode()
+        {
+            m_eAccuracity = AccuracityMode.almostMiss;
+            return this;
+        }
+        public CAlienCalculator lightHit()
+        {
+            m_eAccuracity = AccuracityMode.lightHit;
+            return this;
+        }
+        public CAlienCalculator strongHit()
+        {
+            m_eAccuracity = AccuracityMode.strongHit;
+            return this;
+        }
+        public CAlienCalculator criticalHit()
+        {
+            m_eAccuracity = AccuracityMode.criticalHit;
+            return this;
+        }
+
+        public string TimeToKillWithFullArmor()
+        {
+            string timeToKill = "";
+            timeToKill = TimeToKillWithVariableArmor(100);
+            return timeToKill;
+        }
+        public string TimeToKillWithZeroArmor()
+        {
+            string timeToKill = "";
+            timeToKill = TimeToKillWithVariableArmor(0);
+            return timeToKill;
+        }
+        public string TimeToKillWithVariableArmor(int armor)
+        {
+            string timeToKill = "";
+
+            return timeToKill;
+        }
+        
     }
 }

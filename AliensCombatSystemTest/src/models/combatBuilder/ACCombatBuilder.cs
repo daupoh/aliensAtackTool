2eed0745b82b632a0dbc2555cc835c384b4fb73a
daupoh <daupoh@DAUPOHPC>
2019-04-15 21:57:46 +0300

    add combat builder interface and abstract class

---
+++ AliensCombatSystemTest/src/models/combatBuilder/ACCombatBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AliensCombatSystemTest.src.models.weapon;
+
+namespace AliensCombatSystemTest.src.models.combatBuilder
+{
+    abstract class ACCombatBuilder : ACEntity, ICombatBuilder
+    {
+        protected IList<ICombatEntity> m_lsCombatEntities;
+
+        private ACCombatBuilder (string name):base(name)
+        {
+            m_lsCombatEntities = new List<ICombatEntity>();
+        }
+
+        public IList<ICombatEntity> CombatEntities
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ AliensCombatSystemTest/src/models/combatBuilder/ICombatBuilder.cs
@@ -0,0 +1,15 @@
+﻿using AliensCombatSystemTest.src.models.weapon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliensCombatSystemTest.src.models.combatBuilder
+{
+    interface ICombatBuilder
+    {
+        IList<ICombatEntity> CombatEntities { get; }
+        void CreateCombatEntityByName(string name);
+    }
+}
